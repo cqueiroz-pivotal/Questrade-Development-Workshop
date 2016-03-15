777b0d1723b9c44dd36f0016328ee4e7c003a9e7
Daniel L. Buchko <dbuchko@pivotal.io>
2016-03-15 15:11:40 -0400

    Changed from postgres to mysql.

--- Spring_Service/simple-jpa-data-pcf-application/src/main/java/io/pivotal/workshop/questrade/model/Customer.java
+++ Spring_Service/simple-jpa-data-pcf-application/src/main/java/io/pivotal/workshop/questrade/model/Customer.java
@@ -2,6 +2,7 @@ package io.pivotal.workshop.questrade.model;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
