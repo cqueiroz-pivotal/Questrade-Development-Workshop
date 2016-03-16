1f96b9348d846856f9f2e230612156c1cc987ac7
Daniel L. Buchko <dbuchko@pivotal.io>
2016-03-16 15:18:45 -0400

    Create student spaces and quota.

---
+++ PCF_Setup/seedpcfserver.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+## create demo users
+##
+## Parameters
+## Usage:
+## $ seedpcf.sh
+##
+## This script assumes you are already logged into your PCF instance.
+## You must be an org manager.
+## It will create 15 spaces in the current org, from "student1" to 
+## "student15", and create and assign a quota "workshop" to each. 
+## Each student account must be created by an admin user.
+##
+
+org="pivot-lshannon"
+#pword="questrade16"
+cf target -o $org -s questrade
+
+cf create-space-quota workshop -m 2G -r 1000 -s 10
+
+i="1"
+## create personal accounts
+while [ $i -lt 11 ]
+do
+# No permissions in PEZ to create users
+#cf create-user student$[$i] $pword
+cf create-space student$[$i]
+cf set-space-quota student$[$i] workshop
+
+# Can only do this after the student accounts have been created
+#cf set-space-role student$[$i] $org student$[$i] SpaceDeveloper
+
+i=$[$i+1]
+done
+
