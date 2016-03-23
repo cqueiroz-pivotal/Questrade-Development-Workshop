4e52d65a9ee94e68b7d96ff5b505349e4cd26042
Daniel L. Buchko <dbuchko@pivotal.io>
2016-03-23 15:27:09 -0400

    Eureka discovery added.

---
+++ Eureka_Service/customer-service-application-eureka-solution/src/main/java/io/pivotal/workshop/questrade/CustomerServiceApplication.java
@@ -0,0 +1,54 @@
+package io.pivotal.workshop.questrade;
+
+import io.pivotal.workshop.questrade.model.Customer;
+import io.pivotal.workshop.questrade.repo.CustomerRepository;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class CustomerServiceApplication {
+	
+	private static final Logger log = LoggerFactory.getLogger(CustomerServiceApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerServiceApplication.class, args);
+	}
+	
+	@Bean
+	public CommandLineRunner demo(CustomerRepository repository) {
+		return (args) -> {
+			
+			//clean up old
+			repository.deleteAll();
+			
+			// save a couple of customers
+			repository.save(new Customer("Dan", "Buchko"));
+			repository.save(new Customer("Luke", "Shannon"));
+			repository.save(new Customer("David 'Maniac'", "Barry"));
+			repository.save(new Customer("Louis", "Lo"));
+
+			// fetch all customers
+			log.info("Customers found with findAll():");
+			log.info("-------------------------------");
+			for (Customer customer : repository.findAll()) {
+				log.info(customer.toString());
+			}
+            log.info("");
+
+			// fetch customers by last name
+			log.info("Customer found with findByLastName('Lo'):");
+			log.info("--------------------------------------------");
+			for (Customer bauer : repository.findByLastName("Lo")) {
+				log.info(bauer.toString());
+			}
+            log.info("");
+		};
+	}
+}
