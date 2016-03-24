0693b915ff829e0d14c65a117deaacab617131be
lshannonPivotal <lshannon@pivotal.io>
2016-03-23 20:13:31 -0400

    Working on that VCAP connector

--- NET_Service/OrderService/OrderService/CloudSqlConnection.cs
+++ NET_Service/OrderService/OrderService/CloudSqlConnection.cs
@@ -5,6 +5,9 @@ using System.Web;
 
 namespace OrderService
 {
+    /**
+     * This class is used to actually get the connection details from VCAPS otherwise look it up in Web.config
+     * */
     public class CloudSqlConnection
     {
         private static String actualConnection = String.Empty;
@@ -23,9 +26,10 @@ namespace OrderService
 
         private static void loadConnectionString()
         {
+            //if running outside PCF this will be NULL
             if (Environment.GetEnvironmentVariable("VCAP_SERVICES") != null)
             {
-                Dictionary<string, object> vcapServices = Newtonsoft.Json.JsonConvert.DeserializeObject<Dictionary<string, object>>(
+                Dictionary<string, object> vcapServices = Newtonsoft.Json.JsonConvert.DeserializeObject<Dictionary<string, object>>( 
                     Environment.GetEnvironmentVariable("VCAP_SERVICES"));
                
                 if (vcapServices != null)
@@ -34,12 +38,12 @@ namespace OrderService
                     var credentials = ((Newtonsoft.Json.Linq.JArray)vcapServices["p-mysql"]).First()["credentials"];
                     if (credentials != null)
                     {
-                        //this is what the connection string looks like
-                        //Server=localhost;Database=OrderDB;Uid=root;Pwd=password;
-                        actualConnection = "Server =" + (String)credentials["hostname"] +"; Database = OrderDB; Uid =" + root"; Pwd =" + password +";" 
+                        //this is what the connection string looks like : Server=localhost;Database=OrderDB;Uid=root;Pwd=password;
+                        actualConnection = "Server =" + credentials["hostname"] + "; Port =" + credentials["port"] + ";  Database = OrderDB; Uid =" + credentials["username"] + "; Pwd =" + credentials["password"] + ";"; 
                     }
                 }
             }
+            //look in Web.conf otherwise
             else
             {
                 actualConnection = "name=OrderDBConnectionString";
